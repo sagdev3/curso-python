803c38b22ef71cabb0e38357853422fb44374711
sagsilver <sagdevweb@gmail.com>
2025-03-26 21:33:35 -0500

    Continuo estudiando

--- ejercicios_chatgpt/fuciones_y_exepciones.py
+++ ejercicios_chatgpt/fuciones_y_exepciones.py
@@ -1,15 +1,31 @@
-def dividir(a, b):
-    try:
-        print(a/b)
-    except ZeroDivisionError:
-        print("No se puede dividir para 0")
-    except ValueError:
-        print('Solo se pueden ingresar numeros')
+# def dividir(a, b):
+#     try:
+#         print(a/b)
+#     except ZeroDivisionError:
+#         print("No se puede dividir para 0")
+#     except ValueError:
+#         print('Solo se pueden ingresar numeros')
 
+
+# try:
+#     a = float(input('Ingrese un numero '))
+#     b = float(input('Ingrese un numero '))
+#     dividir(a, b)
+# except ValueError:
+#     print('solo ingrese numeros')
+
+print("INGRESA UN RANGO DE NUMEROS")
 
 try:
-    a = float(input('Ingrese un numero '))
-    b = float(input('Ingrese un numero '))
-    dividir(a, b)
+    inicio = int(input("Ingresa el inicio del rango: "))
+    fin = int(input("Ingresa hasta donde llega el rango: "))
+    if fin < inicio:
+        print("El valor del inicio debe ser menor que el del final")
+    else:
+        rango = range(inicio, fin+1)
+        # squared = list(map(lambda x: x**2, rango))
+        for x in rango:
+            print(f"{x}² = {x**2}")
+        # print(squared)
 except ValueError:
-    print('solo ingrese numeros')
+    print("Ingresa numeros no caracteres ni letras")
--- ejercicios_chatgpt/gestor_de_tareas.py
+++ ejercicios_chatgpt/gestor_de_tareas.py
@@ -1,1 +1,41 @@
 
+tareas = []
+
+
+def total_tareas():
+    while True:
+        print("Seleccione una Opcion: ")
+        print("1 Agregar tarea")
+        print("2 Ver tareas")
+        print("3 Marcar como completada")
+        print("4 Eliminar tarea")
+        print("5 Salir")
+
+        opcion = input('Escoja una opcion: ')
+
+        if opcion == '5':
+            print("Muchas gracias...")
+            break
+        elif opcion == '1':
+            agregar_tarea(tareas)
+        elif opcion == '2':
+            ver_tareas(tareas)
+
+
+def agregar_tarea(lista):
+    tarea = input("Esscribe una nueva tarea: ")
+    lista.append({"tarea": tarea, "completada": False})
+    print("✅ Tarea agregada")
+
+
+def ver_tareas(lista):
+    if not lista:
+        print("📭 No hay tareas registradas.")
+    else:
+        print("\n📋 Lista de tareas:")
+        for i, t in enumerate(lista, start=1):
+            estado = "✔️" if t["completada"] else "❌"
+            print(f"{i}. {t['tarea']} [{estado}]")
+
+
+total_tareas()
